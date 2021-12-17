b96a7b2b9df86540f432fb2c231ae3e85cc903b2
JaeHaKang <jhkang3457@naver.com>
2021-12-17 13:08:10 +0900

    전역변수 적용X -> 스크립트 호출 순서 변경

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,9 @@
-const API_KEY = apiKey;
-
+const API_KEY = apiKey.weather_api;
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log('You live in', lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  console.log(url);
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
