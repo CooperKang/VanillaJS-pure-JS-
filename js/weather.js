3e946199d1213c488de7c45c7cc0f08b2e902e8e
JaeHaKang <jhkang3457@naver.com>
2021-12-17 13:00:33 +0900

    api key 숨기기

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,16 @@
-const API_KEY = "5e1122011e75c331af5e6148d73766b9";
+const API_KEY = apiKey;
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log("You live in", lat, lon);
+  console.log('You live in', lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃ . `;
     });
